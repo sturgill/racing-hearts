43958cb10854019320718d258bfaaea612962842
Chris Sturgill <chris@thesturgills.com>
2015-02-13 15:42:00 -0600

    Artifact commit

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,6 +1,4 @@
 class StatsController < ApplicationController
-  extend MoneyRails::ActionViewExtension
-
   def index
     render json: json_attrs
   end
@@ -9,7 +7,7 @@ class StatsController < ApplicationController
 
   def json_attrs
     {
-      hearts: self.humanized_money(current_user.hearts),
+      hearts: current_user.hearts_cents.to_f / 100,
       perfumes: current_user.perfumes,
       roses: current_user.roses,
       chocolates: current_user.chocolates,
