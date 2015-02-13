9d7054f263c0450e16b2d4c09c1db488e83abfc6
Chris Sturgill <chris@thesturgills.com>
2015-02-13 13:30:39 -0600

    Unique index on users.email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150213183816) do
+ActiveRecord::Schema.define(version: 20150213192958) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                   limit: 255
@@ -29,5 +29,7 @@ ActiveRecord::Schema.define(version: 20150213183816) do
     t.datetime "updated_at",                                          null: false
     t.string   "name",                    limit: 255
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
