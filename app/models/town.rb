72205b05d9eb2af9c0e20c05be24df2d07f61451
Kevin Spector <kspector7@gmail.com>
2015-02-13 13:33:41 -0500

    adding town logic

--- app/models/town.rb
+++ app/models/town.rb
@@ -1,4 +1,17 @@
 #  :name   (string)
 #  :links  (string of array?)
+
 class Town
+  ALL_TOWNS = %w(A B C D E F)
+  attr_accessor :name
+
+  def initialize(name)
+    self.name = name
+  end
+
+  def neighbors
+    current_index = ALL_TOWNS.index(self.name)
+    return [ALL_TOWNS[current_index-1], ALL_TOWNS[current_index+1]]
+  end
+
 end
