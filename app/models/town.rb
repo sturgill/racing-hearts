d1f947bd74d4a89854b478ec12ecd367bfaaba08
Kevin Spector <kspector7@gmail.com>
2015-02-13 13:45:28 -0500

    world model

--- app/models/town.rb
+++ app/models/town.rb
@@ -1,17 +1,26 @@
 #  :name   (string)
 #  :links  (string of array?)
 
-class Town
+class World
   ALL_TOWNS = %w(A B C D E F)
+
+  attr_accessor :towns
+
+  def initialize
+    self.towns = ALL_TOWNS.map{ |name| Town.new(name) }
+  end
+
+  def neighbors_for_town(current_town_name) 
+    current_index = towns.index {|t| t.name == current_town_name}
+    return [towns[current_index-1].name, towns[current_index+1].name]
+  end
+end
+
+class Town
   attr_accessor :name
 
   def initialize(name)
     self.name = name
-  end
-
-  def neighbors
-    current_index = ALL_TOWNS.index(self.name)
-    return [ALL_TOWNS[current_index-1], ALL_TOWNS[current_index+1]]
   end
 
 end
