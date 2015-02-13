b951ad9ee8d9d623772ff07dc6f564beabb7d822
Chris Sturgill <chris@thesturgills.com>
2015-02-13 12:47:23 -0600

    First stab with auth routes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150213181659) do
+ActiveRecord::Schema.define(version: 20150213183816) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                   limit: 255
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150213181659) do
     t.string   "valentine_identifier",    limit: 255
     t.datetime "created_at",                                          null: false
     t.datetime "updated_at",                                          null: false
+    t.string   "name",                    limit: 255
   end
 
 end
